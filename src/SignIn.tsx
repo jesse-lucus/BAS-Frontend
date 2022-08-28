c32899a32b5ce72fd094fe44d9bf3e97bb8943cc
John31337 <john3133747@outlook.com>
2022-08-29 05:45:40 +0800

    access token

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -52,7 +52,7 @@ export default function SignIn() {
     if(email && password)
       await userAction.signIn(email, password)?.then(result => {
         if(result.success) {
-            localStorage.setItem("token", "successfully"); 
+            localStorage.setItem("token", result.token); 
             if(email) {localStorage.setItem("email", email);}
             push("/main"); 
         }
